573a44d1f3f46cbc4b024ff8b18653ad1d9f90a9
Luka <lukabrkovic@skiff.com>
2023-12-09 11:29:42 +0100

    [perf] logger,recover middleware, routes error handaling

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func (app *application) routes() *chi.Mux {
 	r := chi.NewRouter()
+	
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.NotFound(app.notFoundResponse)
+	r.MethodNotAllowed(app.methodNotAllowedResponse)
 
 	r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		res := map[string]string{
